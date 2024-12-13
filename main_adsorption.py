9b45a15db95b878440a09b47c5fade6007cba8c1
123rui-gao <1801244379@qq.com>
2024-12-13 16:37:34 +0800

    Update main_adsorption.py
    I have corrected the function of calculating the unit cell, now it can avoid the error of inappropriate  number of unit cells used.

--- main_adsorption.py
+++ main_adsorption.py
@@ -97,31 +97,57 @@ class RASPA_Output_Data():
 
 
 def get_unit_cell(cif_location, cutoff):
+
+    # 从cif文件中读取晶格常数
+    flag = [
+        '_cell_length_a', '_cell_length_b', '_cell_length_c',
+        '_cell_angle_alpha', '_cell_angle_beta', '_cell_angle_gamma'
+    ]
+    flag_length = len(flag) 
     with open(cif_location, 'r') as f:
-        text = f.readlines()
-    for i in text:
-        if (i.startswith('_cell_length_a')):
-            a = float(i.split()[-1].strip().split('(')[0])
-        elif (i.startswith('_cell_length_b')):
-            b = float(i.split()[-1].strip().split('(')[0])
-        elif (i.startswith('_cell_length_c')):
-            c = float(i.split()[-1].strip().split('(')[0])
-        elif (i.startswith('_cell_angle_alpha')):
-            alpha = float(i.split()[-1].strip().split('(')[0])
-        elif (i.startswith('_cell_angle_beta')):
-            beta = float(i.split()[-1].strip().split('(')[0])
-        elif (i.startswith('_cell_angle_gamma')):
-            gamma = float(i.split()[-1].strip().split('(')[0])
-            break
-    pi = 3.1415926
+        i = 1
+        flag_count = 0
+        while i <= 5e4:
+            flag_content = flag[flag_count]
+            line = f.readline()
+            if flag_content in line:
+                if flag_content == '_cell_length_a':
+                    a = float(line.strip().split()[1])
+                elif flag_content == '_cell_length_b':
+                    b = float(line.strip().split()[1])
+                elif flag_content == '_cell_length_c':
+                    c = float(line.strip().split()[1])
+                elif flag_content == '_cell_angle_alpha':
+                    alpha = float(line.strip().split()[1]) * math.pi / 180 # 计算时用弧度单位    
+                elif flag_content == '_cell_angle_beta':
+                    beta = float(line.strip().split()[1]) * math.pi / 180   # 计算时用弧度单位
+                elif flag_content == '_cell_angle_gamma':
+                    gamma = float(line.strip().split()[1]) * math.pi / 180 # 计算时用弧度单位
+                else:
+                    print(f'No specific operation for flag_content: {flag_content}!')
+                
+                flag_count += 1
+                if flag_count == flag_length:
+                    break
+            i += 1
 
-    a_length = a * math.sin(beta / 180 * pi)
-    b_length = b * math.sin(gamma / 180 * pi)
-    c_length = c * math.sin(alpha / 180 * pi)
+    # 计算晶胞体积          
+    V =  a * b * c * (1 + 2 * math.cos(alpha) * math.cos(beta) * math.cos(gamma) - (math.cos(alpha))**2 - (math.cos(beta))**2 - (math.cos(gamma))**2) ** 0.5
+    
+    # 计算晶胞各个面的表面积
+    base_area_x = b * c * math.sin(alpha)
+    base_area_y = a * c * math.sin(beta)
+    base_area_z = a * b * math.sin(gamma)
 
-    a_unitcell = int(2 * cutoff / a_length + 1)
-    b_unitcell = int(2 * cutoff / b_length + 1)
-    c_unitcell = int(2 * cutoff / c_length + 1)
+    # 计算各个方向的最小距离，即平行六面体各个方向的高，等于体积除以底面积
+    perpendicular_length_x = V / base_area_x
+    perpendicular_length_y = V / base_area_y
+    perpendicular_length_z = V / base_area_z
+
+    # 根据截断半径r_cut计算所需各方向的unit_cell数目
+    a_unitcell = math.ceil(2 * cutoff / perpendicular_length_x)
+    b_unitcell= math.ceil(2 * cutoff / perpendicular_length_y)
+    c_unitcell= math.ceil(2 * cutoff / perpendicular_length_z)
 
     return "{} {} {}".format(a_unitcell, b_unitcell, c_unitcell)
 
